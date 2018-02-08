5e03007ed0a0889c2dae7c6a2b31df5a60bcc4d9
Manabu Hirano <hirano@toyota-ct.ac.jp>
2018-02-08 11:36:10 +0900

    Add download.sh

---
+++ logdrive_orig/download/download.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo downloading bridge-utils-1.5-3.fc17.src.rpm
+wget http://archives.fedoraproject.org/pub/archive/fedora/linux/releases/17/Fedora/source/SRPMS/b/bridge-utils-1.5-3.fc17.src.rpm
+
+echo downloading xen-4.1.2.tar.gz
+wget http://www.xenproject.org/downloads/xen-archives/supported-xen-41-series/xen-412/21-xen-412/file.html
+mv file.html xen-4.1.2.tar.gz
+
+echo downloading CentOS5.11 ISO image
+wget http://ftp.iij.ad.jp/pub/linux/centos-vault/5.11/isos/i386/CentOS-5.11-i386-bin-DVD-1of2.iso
+
+
