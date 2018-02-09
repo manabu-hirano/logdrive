0b8a913cc825b0ee15588bb887736f5f2f5ef7f9
Manabu Hirano <hirano@toyota-ct.ac.jp>
2018-02-09 20:39:04 +0900

    Added 3-setup-network.sh

---
+++ setup/3-setup-network.sh
@@ -0,0 +1,63 @@
+cat << EOF
+##
+## This script sets up
+##  - service of xencommons
+##  - ifcfg-eth0 and ifcfg-xenbr0
+##
+EOF
+
+## EDIT HERE IF YOUR NIC IS NOT eth0
+NET_IF=eth0
+
+echo "--- This script update network config file for ${NET_IF}"
+echo "--- Is your network interface ${NET_IF}?"
+read -p "y or n) " yn
+case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) echo "Please edit the head of this file first"; exit;;
+    * ) echo "Please answer y(es) or n(o)."; exit;;
+esac
+
+ETH0_CFG_SRC=./network-settings/ifcfg-${NET_IF}
+XENBR0_CFG_SRC=./network-settings/ifcfg-xenbr0
+NETWORK_CFG_DST=/etc/sysconfig/network-scripts
+
+echo "--- Start xencoomns as an automatic daemon"
+service xencommons start
+chkconfig xencommons on
+
+echo "--- Are you sure to copy ${ETH0_CFG_SRC} "
+echo "---  in ${NETWORK_CFG_DST}?"
+cat ${ETH0_CFG_SRC}
+
+read -p "y or n) " yn
+case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) echo "Please edit ${ETH0_CFG_SRC} first"; exit;;
+    * ) echo "Please answer y(es) or n(o)."; exit;;
+esac 
+
+echo "--- Are you sure to copy ${XENBR0_CFG_SRC} "
+echo "---  in ${NETWORK_CFG_DST}?"
+cat ${XENBR0_CFG_SRC}
+
+read -p "y or n) " yn
+case $yn in
+    [Yy]* ) break;;
+    [Nn]* ) echo "Please edit ${XENBR0_CFG_SRC} first"; exit;;
+    * ) echo "Please answer y(es) or n(o)."; exit;;
+esac
+
+echo "--- Copying ${ETH0_CFG_SRC} and ${XENBR0_CFG_SRC}"
+echo "---  in ${NETWORK_CFG_DST}"
+cp -n ${NETWORK_CFG_DST}/ifcfg-${NET_IF} ${NETWORK_CFG_DST}/ifcfg-${NET_IF}.bak
+cp -n ${NETWORK_CFG_DST}/ifcfg-xenbr0 ${NETWORK_CFG_DST}/ifcfg-xenbr0.bak
+
+cp -f ${ETH0_CFG_SRC} ${NETWORK_CFG_DST}/
+cp -f ${XENBR0_CFG_SRC} ${NETWORK_CFG_DST}/
+
+echo "--- Restarting network services"
+service network restart
+
+echo "--- Finished $0"
+
